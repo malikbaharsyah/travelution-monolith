181c8900ffe63f1c27a6b609f103cf4c012a7f2e
jason <13521008@std.stei.itb.ac.id>
2023-10-06 17:26:13 +0700

    feat: add responsive landing page dashboard ubah landing page sama dashboard jadi interaktif

--- app/views/landingpage.php
+++ app/views/landingpage.php
@@ -30,27 +30,19 @@
         <h2>"Tentukan Perjalanan Sesuai Keinginanmu"</h2>
     </div>
     <div id="information">
-        <div class="info">
-            <div class="infosekilas">
-                <h1>Sekilas Tentang Kami</h1>
-                <p>Travelution adalah sebuah website yang membantu pelayanan kebutuhan travelling untuk masyarakat luas yang ada di seluruh Indonesia.
-                    Terdapat berbagai macam layanan yang tersedia di website ini, seperti pemesanan tiket pesawat, pemesanan hotel, dan pemesanan paket wisata.
-                    Dengan adanya website ini, diharapkan dapat membantu masyarakat dalam memenuhi kebutuhan travelling.
-                </p>
-            </div>
-            <div class="infofitur">
-                <h1>Fitur:</h1>
-                <ol>
-                    <li>Rekomendasi Destinasi Wisata</li>
-                    <li>Rekomendasi Kebutuhan Akomodasi</li>
-                    <li>Pembuatan Personal Travel Plan Mandiri</li>
-                    <li>Rating dan Review Destinasi Wisata</li>
-                </ol>
-            </div>
+        <div class="infosekilas">
+            <h1>Sekilas Tentang Kami</h1>
+            <p>
+                Travelution adalah sebuah platform online yang telah didedikasikan untuk memenuhi berbagai kebutuhan perjalanan bagi masyarakat luas di seluruh Indonesia. Dengan layanan yang komprehensif, pengguna dapat dengan mudah melakukan pemesanan tiket pesawat untuk perjalanan domestik atau internasional, mencari dan memesan akomodasi hotel sesuai dengan preferensi mereka, serta merencanakan petualangan tak terlupakan dengan pilihan pemesanan paket wisata yang beragam. Website ini dirancang dengan tujuan utama untuk memberikan kemudahan dan kenyamanan dalam merencanakan dan melaksanakan perjalanan, sekaligus membantu masyarakat menjelajahi berbagai destinasi menarik tanah air. Dengan Travelution, pengguna dapat memiliki pengalaman perjalanan yang lebih lancar dan dapat fokus sepenuhnya pada kesenangan dan petualangan mereka, tanpa harus khawatir tentang berbagai detail teknis yang terkait dengan perjalanan.
+                <br>
+                <br>
+                Website Travelution juga berupaya untuk memberikan berbagai opsi yang terjangkau dan beragam, sehingga setiap individu dapat menemukan solusi yang sesuai dengan anggaran dan kebutuhan mereka. Dengan demikian, Travelution bukan hanya sekadar platform pemesanan, tetapi juga mitra setia dalam menjadikan impian perjalanan menjadi kenyataan. Semua ini bertujuan untuk memfasilitasi perjalanan yang lebih mudah, menyenangkan, dan berkesan bagi semua orang, dengan harapan bahwa pengguna akan dapat mengeksplorasi keindahan alam dan budaya yang luar biasa di seluruh Indonesia.
+            </p>
         </div>
         <div class="infovideo">
             <h1>Video</h1>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/xAdas5HG-Wo?si=SJxJULHU0kvwSTPe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><br><br>
+            <iframe width="500" height="300" src="https://www.youtube.com/embed/xAdas5HG-Wo?si=SJxJULHU0kvwSTPe" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe width="500" height="300" src="https://www.youtube.com/embed/XjIi1ezhj6s?si=lrm64y5ITQNhK483" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>        
         </div>
     </div>
     <div class="contact">
